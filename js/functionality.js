c02858431ab6ad9550ec114a121c27ad1c49d773
Aaron Smith <aaron@web-presence.biz>
2025-04-16 21:25:44 -0700

    move to top on select

--- js/functionality.js
+++ js/functionality.js
@@ -51,6 +51,7 @@ function grabData(whichAddress) {
 
     } else {
         document.getElementById("titleTarget").innerHTML = "Active feed: " + address;
+        window.location.href = "#";
     }
 
 
@@ -81,7 +82,7 @@ function grabData(whichAddress) {
 
                     obj[i].title = obj[i].title.replaceAll("'", "").replaceAll('"', '');
                     //  obj[i].title = obj[i].description.replaceAll("'", "&#39;").replace('"', '&#34;');
-                    objHTML = objHTML + `<li  class='list-group-item ' data-num='${i}' ><label class=' pointer'  onClick="toggle('${obj[i].title.trim()}')">${obj[i].title}</label><div class='hide' data-toggle='${obj[i].title.trim()}'><p>${obj[i].description}</p><small><i>${obj[i].pubDate} - <a href='${obj[i].guid}' target='_blank'>Article Link</a></i></small></div></li>`
+                    objHTML = objHTML + `<li  class='list-group-item ' data-num='${i}' ><label class=' pointer'  onClick="toggle('${obj[i].title.trim()}')">${obj[i].title}</label><div class='hide' data-toggle='${obj[i].title.trim()}'><p>${obj[i].description}</p><small><i>${obj[i].pubDate} - <a href='${obj[i].link}' target='_blank'>Article Link</a></i></small></div></li>`
 
 
                     document.getElementById("objTarget").innerHTML = objHTML;
