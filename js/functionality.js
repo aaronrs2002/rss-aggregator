54d9f22975e8c4853c95cdd51ff9535b9660f3aa
Aaron Smith <aaron@web-presence.biz>
2025-04-17 10:33:49 -0700

    &#39;8

--- js/functionality.js
+++ js/functionality.js
@@ -62,7 +62,7 @@ function grabData(whichAddress) {
 
                     obj[i].title = obj[i].title.replaceAll("'", "").replaceAll('"', '').replaceAll("'", "");
                     //  obj[i].title = obj[i].description.replaceAll("'", "&#39;").replace('"', '&#34;');
-                    objHTML = objHTML + `<li  class='list-group-item ' data-num='${i}' ><label class=' pointer'  onClick="toggle(\"${obj[i].title}\")">${obj[i].title}</label><div class='hide' data-toggle='${obj[i].title}'><p>${obj[i].description}</p><small><i>${obj[i].pubDate} - <a href='${obj[i].link}' target='_blank'>Article Link</a></i></small></div></li>`
+                    objHTML = objHTML + `<li  class='list-group-item ' data-num='${i}' ><label class=' pointer'  onClick="toggle(\"${obj[i].title}\")">${obj[i].title}</label><div class='hide' data-toggle=\"${obj[i].title}\"><p>${obj[i].description}</p><small><i>${obj[i].pubDate} - <a href='${obj[i].link}' target='_blank'>Article Link</a></i></small></div></li>`
 
 
                     document.getElementById("objTarget").innerHTML = objHTML;
